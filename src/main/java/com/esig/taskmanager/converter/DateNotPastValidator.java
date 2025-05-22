56daccd4a8852282930e9c4dbbf98141deec1843
Wenderson Gomes <wendersongomes8901@gmail.com>
2025-05-22 19:25:15 -0300

    feat: DateNotPastValidator

---
+++ src/main/java/com/esig/taskmanager/converter/DateNotPastValidator.java
@@ -0,0 +1,21 @@
+package com.esig.taskmanager.converter;
+
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.component.UIComponent;
+import jakarta.faces.context.FacesContext;
+import jakarta.faces.validator.FacesValidator;
+import jakarta.faces.validator.Validator;
+import jakarta.faces.validator.ValidatorException;
+import java.time.LocalDate;
+
+@FacesValidator("dateNotPastValidator")
+public class DateNotPastValidator implements Validator<LocalDate> {
+    @Override
+    public void validate(FacesContext context, UIComponent component, LocalDate value) {
+        if (value != null && value.isBefore(LocalDate.now())) {
+            throw new ValidatorException(new FacesMessage(FacesMessage.SEVERITY_ERROR,
+                    "A data não pode estar no passado", null));
+        }
+    }
+}
+
